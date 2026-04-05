cbdb152b46e645b2f3833a63c8860509b36a945e
Xushnudbek76 <abdugafforovxushnudbek17@gmail.com>
2026-04-05 10:05:10 +0900

    feat: routes integration

---
+++ src/app/App.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+    return
+    <>
+    <Routes>
+    <Route path="/laptops" />
+    <Route path="/orders" />
+    <Route path="/member-page" />
+    <Route path="/help" />
+    <Route path="/" />
+    </Routes>
+    </> 
+
+}
+
+export default App;
